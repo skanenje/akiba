256630c5de652d84c8c858030412fd90933bfd08
skanenje <swapomuse@gmail.com>
2025-06-10 21:51:21 +0300

    fix: layout issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -168,7 +168,7 @@
           <ul class="space-y-4">
             <li class="flex items-center space-x-3">
               <div class="w-8 h-8 bg-green-500 rounded-lg flex items-center justify-center">
-                <svg class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 24 24" style="width: 1rem; height: 1rem;">
+                <svg class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 24 24" style="width: 1em; height: 1em;">
                   <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
                 </svg>
               </div>
